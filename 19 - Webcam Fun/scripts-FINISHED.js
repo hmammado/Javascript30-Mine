4775f95b20df601a0f5b469e0d8e89214612faa2
Haji Mammadov <hmammado@gmail.com>
2022-03-26 19:56:53 -0400

    Making video, screenshot, filters

--- 19 - Webcam Fun/scripts-FINISHED.js
+++ 19 - Webcam Fun/scripts-FINISHED.js
@@ -8,14 +8,14 @@ function getVideo() {
   navigator.mediaDevices.getUserMedia({ video: true, audio: false })
     .then(localMediaStream => {
       console.log(localMediaStream);
-    
-//  DEPRECIATION : 
+
+//  DEPRECIATION :
 //       The following has been depreceated by major browsers as of Chrome and Firefox.
 //       video.src = window.URL.createObjectURL(localMediaStream);
 //       Please refer to these:
 //       Deprecated  - https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
 //       Newer Syntax - https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
-      
+
       video.srcObject = localMediaStream;
       video.play();
     })
@@ -34,8 +34,8 @@ function paintToCanvas() {
     ctx.drawImage(video, 0, 0, width, height);
     // take the pixels out
     let pixels = ctx.getImageData(0, 0, width, height);
-    // mess with them
-    // pixels = redEffect(pixels);
+     mess with them
+     pixels = redEffect(pixels);
 
     pixels = rgbSplit(pixels);
     // ctx.globalAlpha = 0.8;
--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,84 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+// turn on the camera/ask for access
+// display the video on the video div
+// when you click take photo, addeventlistener to button which should take a photo of the video
+// download photo you've taken if you want
+// canvas.style.width = '800px';
+// canvas.style.height = '800px';
+//video.style.width = '800px';
+//video.style.height = '800px';
+
+function getVideo() {
+  // const facingMode = "user"; // Can be 'user' or 'environment' to access back or front camera (NEAT!)
+  const constraints = {
+    video: true,
+    audio: false
+  };
+
+  /* Stream it to video element */
+  navigator.mediaDevices.getUserMedia(constraints)
+    .then(stream => {
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch(err => alert('Error occurred: ', err));
+}
+
+function displayOnCanvas() {
+  const height = video.videoHeight;
+  const width = video.videoWidth;
+  canvas.height = height;
+  canvas.width = width;
+
+
+ return setInterval(() => {
+   ctx.drawImage(video, 0, 0,width, height);
+   let pixels = ctx.getImageData(0, 0, width, height);
+   pixels = rgbSplit(pixels);
+
+   pixels = redEffect(pixels);
+   ctx.putImageData(pixels, 0, 0);
+  }, 16);
+
+}
+
+function takePhoto() {
+
+ snap.currentTime = 0;
+ snap.play();
+
+ const data = canvas.toDataURL('image/jpeg');
+ const link = document.createElement('a');
+ link.href = data;
+
+ link.setAttribute('download', 'selfie');
+ link.innerHTML = `<img src='${data}' alt="selfie">`;
+ strip.insertBefore(link, strip.firstChild);
+};
+
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+  }
+  return pixels;
+}
+
+
+function rgbSplit(pixels) {
+ for (let i = 0; i < pixels.data.length; i+=4) {
+   pixels.data[i - 150] = pixels.data[i+0];
+   pixels.data[i + 500] = pixels.data[i+1];
+   pixels.data[i - 550] = pixels.data[i+2];
+ }
+ return pixels
+}
+
+getVideo();
+
+video.addEventListener('canplay', displayOnCanvas);
